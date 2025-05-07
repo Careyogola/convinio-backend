f8776398706f8abe2e81760a25b3fb7071c94fbf
Carey Ogola <ogolacarey254@gmail.com>
2025-05-07 17:29:18 +0300

    Setting up

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', function(req, res){
     res.send('Hello Server');
 })
+const port = process.env.PORT || 5000 ;
 
-app.listen(5000, ()=> {
-    console.log('convinio server running on: http://localhost:5000');
+app.listen(port, ()=> {
+    console.log(`convinio server running on: http://localhost:${port}`);
 });
